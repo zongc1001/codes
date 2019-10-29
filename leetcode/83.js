ee9d2a9b78725b6ecf679e7848dfe50a13be34bc
MATHISON <32095921+zongc1001@users.noreply.github.com>
2019-10-29 17:57:51 +0800

    add nowcoder

---
+++ leetcode/83.js
@@ -0,0 +1,12 @@
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+    
+};
